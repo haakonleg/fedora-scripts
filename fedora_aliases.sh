25ec1f8982044cd806d7e1504010b57d85ddcd2e
Håkon Legernæs <haakoleg@tutanota.com>
2018-10-29 23:43:22 +0100

    Fix for alias script

--- fedora_aliases.sh
+++ fedora_aliases.sh
@@ -17,12 +17,12 @@ create_function() {
     if [[ $(cat ~/.bashrc) =~ "function $1()" ]]; then
         echo "Function $1 already exists"
     else
-        printf "\nfunction %s() {\n %s\n}" "$1" "$2" >> ~/.bashrc
+        printf "\nfunction %s() {\n %s\n}\n" "$1" "$2" >> ~/.bashrc
     fi
 }
 
 # Upd: update dnf and flatpaks
-create_alias "upd" "dnf check-update --refresh; if [ $? -eq 100 ]; then sudo dnf upgrade; fi; flatpak update"
+create_alias "upd" "dnf check-update --refresh; if [ \$? -eq 100 ]; then sudo dnf upgrade; fi; flatpak update"
 
 # Aliases for shorter typing
 create_alias ".." "cd .."
@@ -38,7 +38,6 @@ create_alias "ls" "ls -alhF --color=auto"
 create_function "pushall" \
 '   git add -A
     local m
-    echo "Commit message: "
-    read m
+    read -p "Commit message: " m
     git commit -m "$m"
     git push'
