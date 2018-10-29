5d9d41a20c1d5997475296383ef8933f76e554e1
Håkon Legernæs <haakoleg@tutanota.com>
2018-10-29 22:26:04 +0100

    Update backup script

--- backup.sh
+++ backup.sh
@@ -9,42 +9,55 @@ fi
 BACKUPDIR='backup'
 
 vscode_backup() {
-    mkdir -p $BACKUPDIR/vscode
-
-    # Backup config
-    cp "/home/$USER/.config/Code/User/settings.json" "$BACKUPDIR/vscode"
-
-    # Backup extensions
-    echo "$(code --list-extensions)" > "$BACKUPDIR/vscode/extensions"
+    if [[ -d "/home/$USER/.config/Code" ]]; then
+        mkdir -p $BACKUPDIR/vscode
+        # Backup config
+        cp "/home/$USER/.config/Code/User/settings.json" "$BACKUPDIR/vscode"
+        # Backup extensions
+        echo "$(code --list-extensions)" > "$BACKUPDIR/vscode/extensions"
+    fi
 }
 
 vscode_restore() {
-    # Restore config
-    cp -f "$BACKUPDIR/vscode" "/home/$USER/.config/Code/User/settings.json"
-
-    # Restore extensions
-    while read -r extension; do
-        code --install-extension $extension
-    done < "$BACKUPDIR/vscode/extensions"
+    if [[ $(command -v "code") ]]; then
+        # Restore config
+        cp -f "$BACKUPDIR/vscode" "/home/$USER/.config/Code/User/settings.json"
+        # Restore extensions
+        while read -r extension; do
+            code --install-extension $extension
+        done < "$BACKUPDIR/vscode/extensions"
+    fi
 }
 
 gnome_backup() {
     mkdir -p $BACKUPDIR/gnome
-
     # Backup gnome config
     echo "$(dconf dump /)" > "$BACKUPDIR/gnome/gnome"
-
     # Backup extensions
     cp -r "/home/$USER/.local/share/gnome-shell/extensions" "$BACKUPDIR/gnome/"
+}
+
+ssh_backup() {
+    if [[ -d "/home/$USER/.ssh" ]]; then
+        mkdir -p $BACKUPDIR/ssh
+        cp /home/$USER/.ssh/id_* "$BACKUPDIR/ssh/"
+    fi
+}
+
+ssh_restore() {
+    mkdir -p "/home/$USER/.ssh"
+    cp -f $BACKUPDIR/ssh/id_* "/home/$USER/.ssh/"
 }
 
 case "$1" in
     "backup")
         vscode_backup
         gnome_backup
+        ssh_backup
         ;;
     "restore")
         vscode_restore
+        ssh_restore
         ;;
     *)
         echo "Usage: $0 {backup|restore}"
